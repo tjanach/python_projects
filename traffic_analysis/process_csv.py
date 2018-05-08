7ec4c655be46796a5bd18764212cc69f47104c59
Tomasz Janaszka <tomasz.janaszka@orange.com>
2018-05-08 16:09:41 +0200

    code changed into a method

--- traffic_analysis/process_csv.py
+++ traffic_analysis/process_csv.py
@@ -8,32 +8,57 @@ file = "traffic_ex_data.csv"
 
 #print(red_file.head(10))
 
+def get_data_frames(file, log=True):
+    data_frames = {}
+    csvfile = open(file,'r')
+    csvFileArray = []
+    for row in csv.reader(csvfile, delimiter = ';'):
+        if row:
+            if row[1]:
+                csvFileArray.append(row)
 
+    for i in range(0,csvFileArray.__len__()-1, 2):
+        if log:
+            print("loading od flow:",csvFileArray[i+1][0])
+        df = pd.DataFrame({'t':csvFileArray[i][1:-1],'v':csvFileArray[i+1][1:-1]})
+        df.v = pd.to_numeric(df.v)
+        df.v = pd.to_numeric(df.v/1000000)
+        df.t = pd.to_datetime(df.t, format='%Y-%m-%d_%H:%M:%S', errors='coerce')
+        data_frames[csvFileArray[i+1][0]] = df
 
-csvfile = open(file,'r')
-csvFileArray = []
-for row in csv.reader(csvfile, delimiter = ';'):
-    #print(row)
-    if row:
-        #print('not_empty',row)
-        if row[1]:
-            csvFileArray.append(row)
+    csvfile.close()
 
-print(type(csvFileArray))
-print(csvFileArray.__len__())
-for i in range(0,csvFileArray.__len__()-1, 2):
-    print(i,len(csvFileArray[i]))
-    df =pd.DataFrame({'t':csvFileArray[i][1:-1],'v':csvFileArray[i+1][1:-1]})
-    for j in range(0,len(csvFileArray[i])-1):
-        print(csvFileArray[i][j]+'  '+csvFileArray[i+1][j])
-        #print()
+    return data_frames
 
-print(df.t)
-df.v = pd.to_numeric(df.v)
-df.v = pd.to_numeric(df.v/1000000)
-df.t = pd.to_datetime(df.t, format='%Y-%m-%d_%H:%M:%S', errors='coerce')
-print(df.dtypes)
-print(df)
+data_frames = get_data_frames(file)
+for flow_name, df in data_frames.items():
+     print(3*'*',"Flow", flow_name)
+     print(df.v.describe())
+
+# csvfile = open(file,'r')
+# csvFileArray = []
+# for row in csv.reader(csvfile, delimiter = ';'):
+#     #print(row)
+#     if row:
+#         #print('not_empty',row)
+#         if row[1]:
+#             csvFileArray.append(row)
+#
+# print(type(csvFileArray))
+# print(csvFileArray.__len__())
+# for i in range(0,csvFileArray.__len__()-1, 2):
+#     print(i,len(csvFileArray[i]))
+#     df =pd.DataFrame({'t':csvFileArray[i][1:-1],'v':csvFileArray[i+1][1:-1]})
+#     for j in range(0,len(csvFileArray[i])-1):
+#         print(csvFileArray[i][j]+'  '+csvFileArray[i+1][j])
+#         #print()
+#
+# print(df.t)
+# df.v = pd.to_numeric(df.v)
+# df.v = pd.to_numeric(df.v/1000000)
+# df.t = pd.to_datetime(df.t, format='%Y-%m-%d_%H:%M:%S', errors='coerce')
+# print(df.dtypes)
+# print(df)
 
 #print(df.values)
 #for r in csvFileArray:
